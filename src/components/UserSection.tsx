5f6a7b81981e7d7626d4a1ff2273be0af959e751
Dylan-Chambi <dylanchambifi@gmail.com>
2024-05-23 05:28:15 -0400

    Improved user section UX

--- src/components/UserSection.tsx
+++ src/components/UserSection.tsx
@@ -4,7 +4,7 @@ import { signOut } from "@/app/actions";
 import { AuthUser } from "@supabase/supabase-js";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useFormStatus } from "react-dom";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import { IoPerson } from "react-icons/io5";
@@ -17,15 +17,35 @@ interface UserSectionProps {
 
 export default function UserSection({ user }: UserSectionProps) {
   const { pending } = useFormStatus();
-  const [shhowMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        menuRef.current &&
+        buttonRef.current &&
+        !menuRef.current.contains(event.target as Node) &&
+        !buttonRef.current.contains(event.target as Node)
+      ) {
+        setShowMenu(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuRef]);
 
   return (
     <div className="relative ml-3">
-      <div>
         <button
-          onClick={() => setShowMenu(!shhowMenu)}
+          ref={buttonRef}
+          onClick={() => setShowMenu(!showMenu)}
           type="button"
-          className="flex items-center justify-center bg-backgroundSidebar rounded-3xl shadow-sm p-2 border-2 gap-x-2 border-gray-700 focus:outline-none focus:ring-2 focus:border-transparent"
+          className="flex items-center justify-center bg-backgroundSidebar rounded-3xl shadow-sm p-2 border-2 gap-x-2 border-gray-700 focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary transition duration-200 ease-in-out"
           id="user-menu-button"
           aria-expanded="false"
           aria-haspopup="true"
@@ -41,18 +61,19 @@ export default function UserSection({ user }: UserSectionProps) {
           <p className="text-xs font-medium text-onBackground hidden md:block">
             {user.user_metadata.full_name}
           </p>
-          {shhowMenu ? (
+          {showMenu ? (
             <IoIosArrowUp size={20} />
           ) : (
             <IoIosArrowDown size={20} />
           )}
         </button>
-        {shhowMenu && (
+        {showMenu && (
           <div
-            className="absolute right-0 mt-2 w-50 md:w-full bg-backgroundSidebar rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+            className="absolute right-0 mt-2 w-50 md:w-full z-10 bg-backgroundSidebar rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
             role="menu"
             aria-orientation="vertical"
             aria-labelledby="user-menu-button"
+            ref={menuRef}
           >
             <div className="flex flex-col gap-y-1 px-4 py-2" role="none">
               <span className="text-xs text-onBackground">Signed in as</span>
@@ -84,7 +105,6 @@ export default function UserSection({ user }: UserSectionProps) {
             </div>
           </div>
         )}
-      </div>
     </div>
   );
 }
